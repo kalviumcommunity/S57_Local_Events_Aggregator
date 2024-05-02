3dbb02afc29c0ccf745dda12fe41561450b2cb48
Naveenthesparrow <naveenneevan6380@gmail.com>
2024-05-02 15:53:30 +0530

    one entity done

--- Backend/server.js
+++ Backend/server.js
@@ -7,13 +7,6 @@ const port = 3000;
 app.use(express.json());
 app.use("/api", routes);
 connectDB();
-app.get("/", (req, res) => {
-  if (checkConnection()) {
-    res.send("Connected");
-  } else {
-    res.send("Not Connected");
-  }
-});
 app.listen(port, () => {
   console.log("Running on PORT", port);
 });
