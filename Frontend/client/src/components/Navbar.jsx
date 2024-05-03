5172c5153f141ca069992f30d44731913cab8364
Naveenthesparrow <naveenneevan6380@gmail.com>
2024-05-03 17:51:28 +0530

    done

--- Frontend/client/src/components/Navbar.jsx
+++ Frontend/client/src/components/Navbar.jsx
@@ -1,34 +1,95 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import cart from "../images/1.png";
+import search from "../images/2.png";
 
 function Navbar() {
   return (
     <nav>
       <ul className="nav-links">
         <li>
-          <Link to="/Title">Vibe Hub</Link>
+          <Link className="logo" to="/Title">
+            Vibes Hub
+          </Link>
+        </li>
+
+        <li>
+          <Link className="bar" to="/">
+            <div id="btn" class="button">
+              <div id="btn" className="box">
+                H
+              </div>
+              <div id="btn" className="box">
+                O
+              </div>
+              <div id="btn" className="box">
+                M
+              </div>
+              <div id="btn" className="box">
+                E
+              </div>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link to="/">Home</Link>
+          <Link className="bar" to="/">
+            <div class="button">
+              <div className="box">T</div>
+              <div className="box">R</div>
+              <div className="box">E</div>
+              <div className="box">N</div>
+              <div className="box">D</div>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link to="/events-calendar">Events Calendar</Link>
+          <Link className="bar" to="/news">
+            <div class="button">
+              <div className="box">E</div>
+              <div className="box">V</div>
+              <div className="box">E</div>
+              <div className="box">N</div>
+              <div className="box">T</div>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link to="/gallery">Gallery</Link>
+          <div className="search-container">
+            <img src={search} alt="Search" className="searchicon" />
+            <input type="text" placeholder="Search..." className="search-bar" />
+          </div>
         </li>
         <li>
-          <Link to="/news">News</Link>
+          <Link className="bar" to="/contact">
+            <div class="button">
+              <div className="box">U</div>
+              <div className="box">P</div>
+              <div className="box">C</div>
+              <div className="box">O</div>
+              <div className="box">M</div>
+              <div className="box">I</div>
+              <div className="box">N</div>
+              <div className="box">G</div>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link to="/events-parties">Events + Parties</Link>
+          <Link className="bar" to="/contact">
+            <div class="button">
+              <div className="box">C</div>
+              <div className="box">O</div>
+              <div className="box">N</div>
+              <div className="box">T</div>
+              <div className="box">A</div>
+              <div className="box">C</div>
+              <div className="box">T</div>
+              <div className="box">S</div>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact Us</Link>
-        </li>
-        <li>
-          <Link to="/private">Private Us</Link>
+          <img src={cart} />
         </li>
       </ul>
     </nav>
