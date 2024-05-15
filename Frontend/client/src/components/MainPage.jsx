e66c69fa36fa6b43d4ba94bb2b64f70fd5cf87f8
Naveenthesparrow <naveenneevan6380@gmail.com>
2024-05-15 17:15:21 +0530

    done

--- Frontend/client/src/components/MainPage.jsx
+++ Frontend/client/src/components/MainPage.jsx
@@ -11,11 +11,19 @@ function HomeComponent() {
       <div className="container-bg"></div>
       <MySwiper />
       <Event />
+      <div className="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497698.77492875466!2d77.30126945310806!3d12.954459531495921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1715772448157!5m2!1sen!2sin"
+          width="800"
+          height="500"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Google Map"
+        ></iframe>{" "}
+      </div>
       <ContactForm />
-      <p>
-        Welcome to Local Event Aggregator your go-to platform for discovering
-        local events!
-      </p>
     </div>
   );
 }
