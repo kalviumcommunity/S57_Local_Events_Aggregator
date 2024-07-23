617d746b40c51da1a52dac5f8ef785b574b7e537
Naveen <naveenneevan6380@gmail.com>
2024-07-23 10:49:48 +0530

    ga

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./components/LandingPage/HomePage";
 import Navbar from "./components/Navbar";
 import MainPage from "./components/MainPage/MainPage";
@@ -9,26 +9,39 @@ import Event from "./components/Event";
 import ScrollTop from "./components/ScrollTop";
 import SignInSignupWithLocalStorage from "./components/Authentication/SignInout";
 import "./App.css";
+import ReactGA from "react-ga";
+
+const trackingId = "G-SSM973B738"; // Replace with your actual tracking ID
+
+function usePageViews() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+}
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(trackingId); // Initialize GA
+    ReactGA.pageview(window.location.pathname + window.location.search); // Track initial page view
+  }, []);
+
+  usePageViews();
+
   return (
-    <Router>
-      <div>
-        <ScrollTop />
-
-        <Routes>
-          <Route path="/Navbar" element={<Navbar />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/MainPage" element={<MainPage />} />
-          <Route path="/Event" element={<Event />} />
-          <Route path="/MySwiper" element={<MySwiper />} />
-          <Route path="/MainEvent" element={<MainEvent />} />
-
-          {/* Add more routes as needed */}
-        </Routes>
-        <SignInSignupWithLocalStorage />
-      </div>
-    </Router>
+    <div>
+      <ScrollTop />
+      <Routes>
+        <Route path="/Navbar" element={<Navbar />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/MainPage" element={<MainPage />} />
+        <Route path="/Event" element={<Event />} />
+        <Route path="/MySwiper" element={<MySwiper />} />
+        <Route path="/MainEvent" element={<MainEvent />} />
+        {/* Add more routes as needed */}
+      </Routes>
+      <SignInSignupWithLocalStorage />
+    </div>
   );
 }
 
